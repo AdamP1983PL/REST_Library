0010a31ff64f2fa9f5667893e629be8f39a2d017
Adam Podlaski <podlaski83@gmail.com>
2024-01-28 21:03:31 +0100

    mySql configuration

--- src/main/java/com/rest_library/RestLibraryApplication.java
+++ src/main/java/com/rest_library/RestLibraryApplication.java
@@ -2,7 +2,9 @@ package com.rest_library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:mysql.properties")
 @SpringBootApplication
 public class RestLibraryApplication {
 
